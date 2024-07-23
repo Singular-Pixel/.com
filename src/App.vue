e2ae5a8929e3c0083e265fa61c2bef4fe931f56a
Samuel Trimble <sam.trimble@gmail.com>
2024-07-23 14:53:40 -0700

    Initial checkin based on VueViteQuasar template. 'Coming Soon' page.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<QLayout view="hHh lpR fFf">
+		<QPageContainer>
+			<RouterView id="View" />
+		</QPageContainer>
+		<QFooter elevated class="bg-dark">
+			<div class="text-caption text-center titleFont">© 2024 Singular Pixel LLC. All Rights Reserved.</div>
+		</QFooter>
+	</QLayout>
+</template>
+
+<script setup>
+import { onMounted, nextTick } from 'vue';
+
+onMounted(() => {
+	nextTick(Init);
+});
+
+function Init() {
+	console.log(`App init: ${import.meta.env.VITE_BUILD_TARGET}:${import.meta.env.VITE_BUILD_VERSION}:${import.meta.env.MODE}`);
+}
+</script>
+
+<style lang="scss">
+@import '@quasar/extras/mdi-v7/mdi-v7.css';
+@import 'quasar/src/css/index.sass';
+
+@import '@/core/globals';
+@import '@/core/core';
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+	<QPage data-view="Home" class="colorScrollbars">
+		<div class="q-pa-xl row justify-center">
+			<div class="gt-sm col-auto"></div>
+			<div class="col-xs-12 col-md-8 flexCenter vert">
+				<QImg id="logo" class="q-mt-xl" src="@/assets/images/Logo-Vert-OnDark.svg" no-spinner />
+			</div>
+			<div class="gt-sm col-auto"></div>
+		</div>
+		<div class="q-pa-xl row justify-center">
+			<div class="gt-sm col-auto"></div>
+			<div class="col-xs-12 col-md-8">
+				<h1 class="q-my-xl text-h3 titleFont text-center">coming soon</h1>
+			</div>
+			<div class="gt-sm col-auto"></div>
+		</div>
+	</QPage>
+</template>
+
+<script setup>
+import { onMounted, nextTick } from 'vue';
+
+onMounted(() => {
+	nextTick(Init);
+});
+
+function Init() {
+	
+}
+</script>
+
+<style lang="scss">
+@import '@/core/globals';
+
+#View[data-view="Home"] {
+
+	#logo {
+		max-width: 640px;
+	}
+}
+</style>
