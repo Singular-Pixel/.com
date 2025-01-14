77605006fd11d815379c7b81e29dd60c87dd03b2
Samuel Trimble <sam.trimble@gmail.com>
2025-01-13 20:37:26 -0800

    Package & vue config updates

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 //import vueDevTools from 'vite-plugin-vue-devtools';
-import { quasar } from '@quasar/vite-plugin';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		vue(),
+		vue({
+			template: { transformAssetUrls }
+		}),
 //		vueDevTools(),
 		quasar({
 			autoImportComponentCase: 'pascal',
